3049e394e578f86db3f85f09484ce67f200d1e19
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-09 16:20:31 -0500

    Create skeleton.test.js

---
+++ test/skeleton.test.js
@@ -0,0 +1,6 @@
+import {SkeletonBuilder} from 'straight-skeleton';
+
+const skeleton = SkeletonBuilder.BuildFromGeoJSON([[
+	[[0, 0], [100, 0], [100, 50], [0, 50]],    // outer
+	[[50, 30], [70, 30], [70, 20], [50, 20]],  // inner
+]]);
