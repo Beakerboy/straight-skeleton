ae4d801cb99ebe744e2b5fb9ef659a4c706ca92d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-10 12:47:50 -0500

    Update list.test.js

--- test/list.test.js
+++ test/list.test.js
@@ -1,4 +1,7 @@
 import { CircularList } from '../src/Circular/circularlist.js';
 
-const list = new CircularList();
-expect(list.size).toBe(0);
+test('', () => {
+  const list = new CircularList();
+  expect(list.size).toBe(0);
+});
+
