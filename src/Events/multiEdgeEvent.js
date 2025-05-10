86781e88c40af5a7f121514bf7a494709ccd2029
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-10 18:07:55 -0500

    Update multiEdgeEvent.js

--- src/Events/multiEdgeEvent.js
+++ src/Events/multiEdgeEvent.js
@@ -1,17 +1,18 @@
-import SkeletonEvent from "./SkeletonEvent";
-import Vector2d from "../Primitives/Vector2d";
-import EdgeChain from "./Chains/EdgeChain";
+import SkeletonEvent from './SkeletonEvent';
+import Vector2d from '../Primitives/Vector2d';
+import EdgeChain from './Chains/EdgeChain';
 
-export default class MultiEdgeEvent extends SkeletonEvent {
-	public readonly Chain: EdgeChain;
+class MultiEdgeEvent extends SkeletonEvent {
+  public readonly Chain: EdgeChain;
 
-	public override get IsObsolete(): boolean {
-		return false;
-	}
+ IsObsolete() {
+    return false;
+  }
 
-	constructor(point: Vector2d, distance: number, chain: EdgeChain) {
-		super(point, distance);
+  constructor(point, distance, chain) {
+    super(point, distance);
 
-		this.Chain = chain;
-	}
+    this.Chain = chain;
+  }
 }
+export {MultiEdgeEvent};
