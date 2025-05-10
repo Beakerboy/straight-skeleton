25adbf1743922a5ac8d780bbba11b31db585fc7f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-10 18:25:14 -0500

    Update multiSplitEvent.js

--- src/Events/multiSplitEvent.js
+++ src/Events/multiSplitEvent.js
@@ -1,18 +1,19 @@
-import SkeletonEvent from "./SkeletonEvent";
-import {List} from "../Utils";
-import IChain from "./Chains/IChain";
-import Vector2d from "../Primitives/Vector2d";
+import SkeletonEvent from './SkeletonEvent';
+import {List} from '../Utils';
+import IChain from './Chains/IChain';
+import Vector2d from '../Primitives/Vector2d';
 
-export default class MultiSplitEvent extends SkeletonEvent {
-	public readonly Chains: List<IChain>;
+class MultiSplitEvent extends SkeletonEvent {
+  Chains;
 
-	public override get IsObsolete(): boolean {
+	IsObsolete() {
 		return false;
 	}
 
-	constructor(point: Vector2d, distance: number, chains: List<IChain>) {
-		super(point, distance);
+  constructor(point, distance, chains) {
+    super(point, distance);
 
-		this.Chains = chains;
-	}
+    this.Chains = chains;
+  }
 }
+export {MultiSplitEvent};
