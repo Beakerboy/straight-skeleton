b5cfe0b30f281ca9e629323d327902824f4e4a5c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-10 22:18:43 -0500

    Update splitEvent.js

--- src/Events/splitEvent.js
+++ src/Events/splitEvent.js
@@ -4,22 +4,24 @@ import Vertex from '../Circular/Vertex';
 import Vector2d from '../Primitives/Vector2d';
 
 export default class SplitEvent extends SkeletonEvent {
-  public readonly OppositeEdge: Edge = null;
-  public readonly Parent: Vertex = null;
+  //Edge
+  OppositeEdge = null;
+  //Vertex
+  Parent = null;
 
-  constructor(point: Vector2d, distance: number, parent: Vertex, oppositeEdge: Edge) {
+  constructor(point, distance, parent, oppositeEdge) {
     super(point, distance);
 
     this.Parent = parent;
     this.OppositeEdge = oppositeEdge;
   }
 
-  public override get IsObsolete(): boolean {
+  IsObsolete(): boolean {
     return this.Parent.IsProcessed;
   }
 
   
-  ToString(): string {
+  ToString() {
     return 'SplitEvent [V=' + this.V + ', Parent=' + (this.Parent !== null ? this.Parent.Point.ToString() : 'null') +
       ', Distance=' + this.Distance + ']';
   }
