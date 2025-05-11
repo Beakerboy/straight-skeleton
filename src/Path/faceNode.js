7a953f572955017cbe9dfa416ecbac7c9521e553
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-10 22:43:56 -0500

    Update faceNode.js

--- src/Path/faceNode.js
+++ src/Path/faceNode.js
@@ -1,24 +1,24 @@
-import PathQueueNode from "./PathQueueNode";
-import Vertex from "../Circular/Vertex";
-import FaceQueue from "./FaceQueue";
+import PathQueueNode from './PathQueueNode';
+import Vertex from '../Circular/Vertex';
+import FaceQueue from './FaceQueue';
 
-export class FaceNode extends PathQueueNode<FaceNode> {
-	public readonly Vertex: Vertex = null;
+class FaceNode extends PathQueueNode {
+  Vertex = null;
 
-	constructor(vertex: Vertex) {
-		super();
-		this.Vertex = vertex;
-	}
+  constructor(vertex: Vertex) {
+    super();
+    this.Vertex = vertex;
+  }
 
-	public get FaceQueue(): FaceQueue {
-		return <FaceQueue>this.List;
-	}
+  FaceQueue() {
+    return <FaceQueue>this.List;
+  }
 
-	public get IsQueueUnconnected(): boolean {
-		return this.FaceQueue.IsUnconnected;
-	}
+  IsQueueUnconnected() {
+    return this.FaceQueue.IsUnconnected;
+  }
 
-	public QueueClose() {
-		this.FaceQueue.Close();
-	}
+  QueueClose() {
+    this.FaceQueue.Close();
+  }
 }
