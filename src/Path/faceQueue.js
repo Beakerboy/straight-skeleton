da1eb67bfc9179ab244176e46a703f605097f38b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-11 07:28:03 -0500

    Update faceQueue.js

--- src/Path/faceQueue.js
+++ src/Path/faceQueue.js
@@ -3,22 +3,23 @@ import {FaceNode} from "./FaceNode";
 import PathQueueNode from "./PathQueueNode";
 import Edge from "../Circular/Edge";
 
-export default class FaceQueue extends PathQueue<FaceNode> {
-	public Edge: Edge = null;
-	public Closed: boolean = false;
+class FaceQueue extends PathQueue {
+  Edge = null;
+  Closed = false;
 
-	public get IsUnconnected(): boolean {
+  IsUnconnected() {
 		return this.Edge === null;
 	}
 
-	public override AddPush(node: PathQueueNode<FaceNode>, newNode: PathQueueNode<FaceNode>) {
-		if (this.Closed)
-			throw new Error("Can't add node to closed FaceQueue");
+  public override AddPush(node, newNode) {
+    if (this.Closed) {
+      throw new Error("Can't add node to closed FaceQueue");
+    }
+    super.AddPush(node, newNode);
+  }
 
-		super.AddPush(node, newNode);
-	}
-
-	public Close() {
-		this.Closed = true;
-	}
+  Close() {
+    this.Closed = true;
+  }
 }
+export {FaceQueue};
