65153ac5a01b6c0de8371466e6c2a0bfd2e8f0a4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-11 15:10:27 -0500

    Update pathQueueNode.js

--- src/Path/pathQueueNode.js
+++ src/Path/pathQueueNode.js
@@ -1,51 +1,51 @@
-import PathQueue from "./PathQueue";
+import PathQueue from './PathQueue';
 
-export default class PathQueueNode<T extends PathQueueNode<T>> {
-	public List: PathQueue<T> = null;
-	public Next: PathQueueNode<T> = null;
-	public Previous: PathQueueNode<T> = null;
+class PathQueueNode {
+  List = null;
+  Next = null;
+  Previous = null;
 
-	public get IsEnd(): boolean {
-		return this.Next === null || this.Previous === null;
-	}
+  IsEnd(): boolean {
+    return this.Next === null || this.Previous === null;
+  }
 
-	public AddPush(node: PathQueueNode<T>) {
-		this.List.AddPush(this, node);
-	}
+  AddPush(node) {
+    this.List.AddPush(this, node);
+  }
 
-	public AddQueue(queue: PathQueueNode<T>): PathQueueNode<T> {
-		if (this.List === queue.List)
-			return null;
+  AddQueue(queue: PathQueueNode<T>): PathQueueNode<T> {
+    if (this.List === queue.List)
+      return null;
 
-		let currentQueue: PathQueueNode<T> = this;
+    let currentQueue: PathQueueNode<T> = this;
 
-		let current = queue;
+    let current = queue;
 
-		while (current !== null) {
-			const next = current.Pop();
+    while (current !== null) {
+      const next = current.Pop();
 
-			currentQueue.AddPush(current);
-			currentQueue = current;
+      currentQueue.AddPush(current);
+      currentQueue = current;
 
-			current = next;
-		}
+      current = next;
+    }
 
-		return currentQueue;
-	}
+    return currentQueue;
+  }
 
-	public FindEnd(): PathQueueNode<T> {
-		if (this.IsEnd)
-			return this;
+  FindEnd() {
+    if (this.IsEnd)
+      return this;
 
-		let current: PathQueueNode<T> = this;
+    let current: PathQueueNode<T> = this;
 
-		while (current.Previous !== null)
-			current = current.Previous;
+    while (current.Previous !== null)
+    current = current.Previous;
 
-		return current;
-	}
+    return current;
+  }
 
-	public Pop(): PathQueueNode<T> {
-		return this.List.Pop(this);
-	}
+  Pop() {
+    return this.List.Pop(this);
+  }
 }
