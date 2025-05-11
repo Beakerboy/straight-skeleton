9052c773e2c6b6c7e4053046eff4468adf03e5e4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-10 22:24:05 -0500

    Update splitEvent.js

--- src/Events/splitEvent.js
+++ src/Events/splitEvent.js
@@ -19,7 +19,7 @@ export default class SplitEvent extends SkeletonEvent {
   IsObsolete() {
     return this.Parent.IsProcessed;
   }
-  
+
   ToString() {
     return 'SplitEvent [V=' + this.V + ', Parent=' + (this.Parent !== null ? this.Parent.Point.ToString() : 'null') +
       ', Distance=' + this.Distance + ']';
