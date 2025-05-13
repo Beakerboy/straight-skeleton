5d8bfe920d764afb6f0b732e2251178d7010bfb5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 19:00:03 -0400

    Update skeletonbuilder.js

--- src/skeletonbuilder.js
+++ src/skeletonbuilder.js
@@ -1,4 +1,4 @@
-import {Skeleton} from './Skeleton';
+import {Skeleton} from './skeleton';
 import {HashSet, List, IComparer, Dictionary, GeoJSONMultipolygon} from './utils';
 import Vector2d from './Primitives/vector2d';
 import PriorityQueue from './Primitives/priorityqueue';
