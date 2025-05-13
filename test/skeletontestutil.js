258d3286c4ac0dd7a0700a63c5b2f66ec66938c9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 16:46:01 -0400

    Update skeletontestutil.js

--- test/skeletontestutil.js
+++ test/skeletontestutil.js
@@ -1,20 +1,22 @@
-function ContainsEpsilon(List<Vector2d> list, Vector2d p) {
-  return list.Any(l => EqualEpsilon(l.X, p.X) && EqualEpsilon(l.Y, p.Y));
-}
-
-/**
- * @param
- * @return
- */
-function getFacePoints(sk) {
-  let ret = new List();
-
-  foreach (edgeOutput in sk.Edges) {
-    points = edgeOutput.Polygon;
-    foreach (vector2d in points) {
-      if (!ContainsEpsilon(ret, vector2d))
-        ret.add(vector2d);
-    }
+export default class SkeletinTestUtil {
+  function ContainsEpsilon(List<Vector2d> list, Vector2d p) {
+    return list.Any(l => EqualEpsilon(l.X, p.X) && EqualEpsilon(l.Y, p.Y));
   }
-  return ret;
+
+  /**
+   * @param
+   * @return
+   */
+  function getFacePoints(sk) {
+    let ret = new List();
+
+    foreach (edgeOutput in sk.Edges) {
+      points = edgeOutput.Polygon;
+      foreach (vector2d in points) {
+        if (!ContainsEpsilon(ret, vector2d))
+          ret.add(vector2d);
+      }
+    }
+    return ret;
+  }
 }
