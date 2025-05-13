14cc20d6e88f346832e789fcb1666c30321c422b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 12:54:07 -0400

    ws

--- src/Utils.js
+++ src/Utils.js
@@ -1,40 +1,40 @@
 function insertInArray(array, index, item) {
-	const items = Array.prototype.slice.call(arguments, 2);
+  const items = Array.prototype.slice.call(arguments, 2);
 
-	return [].concat(array.slice(0, index), items, array.slice(index));
+  return [].concat(array.slice(0, index), items, array.slice(index));
 }
 
 export class List {
   arr;
-	constructor(capacity = 0) {
-		this.arr = [];
-	}
+  constructor(capacity = 0) {
+    this.arr = [];
+  }
 
-	Add(item) {
-		this.arr.push(item);
-	}
+  Add(item) {
+    this.arr.push(item);
+  }
 
-	Insert(index, item) {
-		const newArr = insertInArray(this.arr, index, item);
+  Insert(index, item) {
+    const newArr = insertInArray(this.arr, index, item);
 
-		this.arr.length = newArr.length;
+    this.arr.length = newArr.length;
 
-		for(let i = 0; i < newArr.length; i++) {
-			this.arr[i] = newArr[i];
-		}
-	}
+    for(let i = 0; i < newArr.length; i++) {
+      this.arr[i] = newArr[i];
+    }
+  }
 
-	Reverse() {
-		this.arr.reverse();
-	}
+  Reverse() {
+    this.arr.reverse();
+  }
 
-	Clear() {
-		this.arr.length = 0;
-	}
+  Clear() {
+    this.arr.length = 0;
+  }
 
-	Count() {
-		return this.arr.length;
-	}
+  Count() {
+    return this.arr.length;
+  }
 
   /**
    * return true if any elements in the array match the filter
@@ -57,61 +57,61 @@ export class List {
   }
 
 
-	RemoveAt(index) {
-		this.arr.splice(index, 1);
-	}
+  RemoveAt(index) {
+    this.arr.splice(index, 1);
+  }
 
-	Remove(itemToRemove) {
-		const newArr = this.arr.filter(item => item !== itemToRemove);
+  Remove(itemToRemove) {
+    const newArr = this.arr.filter(item => item !== itemToRemove);
 
-		this.arr.length = newArr.length;
+    this.arr.length = newArr.length;
 
-		for(let i = 0; i < newArr.length; i++) {
-			this.arr[i] = newArr[i];
-		}
-	}
+    for(let i = 0; i < newArr.length; i++) {
+      this.arr[i] = newArr[i];
+    }
+  }
 
-	AddRange(list) {
-		for (const item of list) {
-			this.Add(item);
-		}
-	}
+  AddRange(list) {
+    for (const item of list) {
+      this.Add(item);
+    }
+  }
 
-	Sort(comparer) {
-		this.arr.sort(comparer.Compare.bind(comparer));
-	}
+  Sort(comparer) {
+    this.arr.sort(comparer.Compare.bind(comparer));
+  }
 }
 
 export class HashSet {
-	Set;
+  Set;
 
-	constructor() {
-		this.Set = new Set();
-	}
+  constructor() {
+    this.Set = new Set();
+  }
 
-	Add(item) {
-		this.Set.add(item);
-	}
+  Add(item) {
+    this.Set.add(item);
+  }
 
-	Remove(item) {
-		this.Set.delete(item);
-	}
+  Remove(item) {
+    this.Set.delete(item);
+  }
 
-	RemoveWhere(filter) {
-		for (const item of this.Set.values()) {
-			if (filter(item)) {
-				this.Set.delete(item);
-			}
-		}
-	}
+  RemoveWhere(filter) {
+    for (const item of this.Set.values()) {
+      if (filter(item)) {
+        this.Set.delete(item);
+      }
+    }
+  }
 
-	Contains(item) {
-		return this.Set.has(item);
-	}
+  Contains(item) {
+    return this.Set.has(item);
+  }
 
-	Clear() {
-		this.Set.clear();
-	}
+  Clear() {
+    this.Set.clear();
+  }
 
   *[Symbol.iterator]() {
     for (const item of this.Set.values()) {
@@ -122,11 +122,11 @@ export class HashSet {
 }
 
 export class Dictionary {
-	ContainsKey(key) {
-		return this.has(key);
-	}
+  ContainsKey(key) {
+    return this.has(key);
+  }
 
-	Add(key, value) {
-		return this.set(key, value);
-	}
+  Add(key, value) {
+    return this.set(key, value);
+  }
 }
