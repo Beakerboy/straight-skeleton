ded6003f354a5e279bf0da83ed18648c9a6267ce
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 15:21:10 -0400

    Update and rename skeleton.test1.js to skeleton.test.js

---
+++ test/skeleton.test.js
@@ -0,0 +1,21 @@
+import {SkeletonBuilder} from '../src/straight-skeleton';
+import {List} from '../src/utils.js';
+import Vector2D from '../src/Primatives/vector2d.js';
+
+test('Circular Add Test', () => {
+
+  polygon = new List(
+    [
+      new Vector2d(50, 50),
+      new Vector2d(100, 50),
+      new Vector2d(100, 100),
+      new Vector2d(50, 100)
+    ];
+
+  var expected = new List([new Vector2d(75.000000, 75.000000)]);
+  expected.AddRange(polygon);
+
+  var sk = SkeletonBuilder.Build(polygon);
+
+  SkeletonTestUtil.AssertExpectedPoints(expected, SkeletonTestUtil.GetFacePoints(sk));
+});
