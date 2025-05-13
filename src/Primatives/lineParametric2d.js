210ec1214bcc234b003f2411cfd72d4c6b17e27f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 13:43:38 -0400

    ws

--- src/Primatives/lineParametric2d.js
+++ src/Primatives/lineParametric2d.js
@@ -3,45 +3,45 @@ import LineLinear2d from './LineLinear2d';
 import PrimitiveUtils from './PrimitiveUtils';
 
 export default class LineParametric2d {
-	static Empty = new LineParametric2d(Vector2d.Empty, Vector2d.Empty);
+  static Empty = new LineParametric2d(Vector2d.Empty, Vector2d.Empty);
 
-	A = null;
-	U = null;
+  A = null;
+  U = null;
 
-	constructor(pA, pU) {
-		this.A = pA;
-		this.U = pU;
-	}
+  constructor(pA, pU) {
+    this.A = pA;
+    this.U = pU;
+  }
 
-	public CreateLinearForm() {
-		const x = this.A.X;
-		const y = this.A.Y;
+  public CreateLinearForm() {
+    const x = this.A.X;
+    const y = this.A.Y;
 
-		const B = -this.U.X;
-		const A = this.U.Y;
+    const B = -this.U.X;
+    const A = this.U.Y;
 
-		const C = -(A * x + B * y);
+    const C = -(A * x + B * y);
 
-		return new LineLinear2d().SetFromCoefficients(A, B, C);
-	}
+    return new LineLinear2d().SetFromCoefficients(A, B, C);
+  }
 
-	public static Collide(ray, line, epsilon) {
-		const collide = LineLinear2d.Collide(ray.CreateLinearForm(), line);
-		if (collide.Equals(Vector2d.Empty)) {
-			return Vector2d.Empty;
-		}
+  public static Collide(ray, line, epsilon) {
+    const collide = LineLinear2d.Collide(ray.CreateLinearForm(), line);
+    if (collide.Equals(Vector2d.Empty)) {
+      return Vector2d.Empty;
+    }
 
-		const collideVector = collide.Sub(ray.A);
-		return ray.U.Dot(collideVector) < epsilon ? Vector2d.Empty : collide;
-	}
+    const collideVector = collide.Sub(ray.A);
+    return ray.U.Dot(collideVector) < epsilon ? Vector2d.Empty : collide;
+  }
 
-	public IsOnLeftSite(point, epsilon) {
-		const direction = point.Sub(this.A);
-		return PrimitiveUtils.OrthogonalRight(this.U).Dot(direction) < epsilon;
-	}
+  public IsOnLeftSite(point, epsilon) {
+    const direction = point.Sub(this.A);
+    return PrimitiveUtils.OrthogonalRight(this.U).Dot(direction) < epsilon;
+  }
 
-	public IsOnRightSite(point, epsilon) {
-		const direction = point.Sub(this.A);
-		return PrimitiveUtils.OrthogonalRight(this.U).Dot(direction) > -epsilon;
-	}
+  public IsOnRightSite(point, epsilon) {
+    const direction = point.Sub(this.A);
+    return PrimitiveUtils.OrthogonalRight(this.U).Dot(direction) > -epsilon;
+  }
 }
