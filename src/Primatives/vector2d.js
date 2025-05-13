28617b9ff7d4ff2928118de98f15c80b6fcf3726
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 14:24:16 -0400

    Update vector2d.js

--- src/Primatives/vector2d.js
+++ src/Primatives/vector2d.js
@@ -1,10 +1,10 @@
 class Vector2d {
 	public static Empty: Vector2d = new Vector2d(Number.MIN_VALUE, Number.MIN_VALUE);
 
-	public X: number = 0;
-	public Y: number = 0;
+	X = 0;
+	Y = 0;
 
-	constructor(x: number, y: number) {
+	constructor(x, y) {
 		this.X = x;
 		this.Y = y;
 	}
@@ -14,48 +14,48 @@ class Vector2d {
 		this.Y = -this.Y;
 	}
 
-	public DistanceTo(var1: Vector2d): number {
+	public DistanceTo(var1) {
 		const var2 = this.X - var1.X;
 		const var4 = this.Y - var1.Y;
 		return Math.sqrt(var2 * var2 + var4 * var4);
 	}
 
-	public Normalized(): Vector2d {
+	public Normalized() {
 		const var1 = 1 / Math.sqrt(this.X * this.X + this.Y * this.Y);
 		return new Vector2d(this.X * var1, this.Y * var1);
 	}
 
-	public Dot(var1: Vector2d): number {
+	public Dot(var1) {
 		return this.X * var1.X + this.Y * var1.Y;
 	}
 
-	public DistanceSquared(var1: Vector2d): number {
+	public DistanceSquared(var1) {
 		const var2 = this.X - var1.X;
 		const var4 = this.Y - var1.Y;
 		return var2 * var2 + var4 * var4;
 	}
 
-	public Add(v: Vector2d): Vector2d {
+	public Add(v) {
 		return new Vector2d(this.X + v.X, this.Y + v.Y);
 	}
 
-	public Sub(v: Vector2d): Vector2d {
+	public Sub(v) {
 		return new Vector2d(this.X - v.X, this.Y - v.Y);
 	}
 
-	public MultiplyScalar(scale: number): Vector2d {
+	public MultiplyScalar(scale): Vector2d {
 		return new Vector2d(this.X * scale, this.Y * scale);
 	}
 
-	public Equals(v: Vector2d): boolean {
+	public Equals(v) {
 		return this.X === v.X && this.Y === v.Y;
 	}
 
-	public NotEquals(v: Vector2d): boolean {
+	public NotEquals(v) {
 		return !this.Equals(v);
 	}
 
-	public ToString(): string {
+	public ToString() {
 		return `${this.X}, ${this.Y}`;
 	}
 }
