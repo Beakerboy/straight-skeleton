cfcf5998cc632ee1abea9ccc31c9a1564c9fd16b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 17:45:38 -0400

    Update skeleton.test.js

--- test/skeleton.test.js
+++ test/skeleton.test.js
@@ -1,6 +1,6 @@
 import {SkeletonBuilder} from '../src/skeletonbuilder.js';
 import {List} from '../src/utils.js';
-import Vector2d from '../src/Primatives/vector2d.js';
+import Vector2d from '../src/Primitives/vector2d.js';
 import SkeletonTestUtil from './skeletontestutil.js';
 
 test('Circular Add Test', () => {
