ffe8800fead2f5920ba174bf4e089749feb85b1a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 13:25:17 -0400

    ws

--- src/lavUtil.js
+++ src/lavUtil.js
@@ -3,54 +3,54 @@ import {List} from './Utils';
 import CircularList from './Circular/CircularList';
 
 export default class LavUtil {
-	static IsSameLav(v1, v2) {
-		if (v1.List === null || v2.List === null)
-			return false;
-		return v1.List === v2.List;
-	}
+  static IsSameLav(v1, v2) {
+    if (v1.List === null || v2.List === null)
+      return false;
+    return v1.List === v2.List;
+  }
 
-	static RemoveFromLav(vertex) {
-		if (vertex === null || vertex.List === null)
-			return;
-		vertex.Remove();
-	}
+  static RemoveFromLav(vertex) {
+    if (vertex === null || vertex.List === null)
+      return;
+    vertex.Remove();
+  }
 
-	static CutLavPart(startVertex, endVertex) {
-		const ret = new List();
-		const size = startVertex.List.Size;
-		let next = startVertex;
+  static CutLavPart(startVertex, endVertex) {
+    const ret = new List();
+    const size = startVertex.List.Size;
+    let next = startVertex;
 
-		for (let i = 0; i < size; i++) {
-			const current = next;
-			next = current.Next;
-			current.Remove();
-			ret.Add(current);
+    for (let i = 0; i < size; i++) {
+      const current = next;
+      next = current.Next;
+      current.Remove();
+      ret.Add(current);
 
-			if (current === endVertex)
-				return ret;
-		}
+      if (current === endVertex)
+        return ret;
+    }
 
-		throw new Error("End vertex can't be found in start vertex lav");
-	}
+    throw new Error("End vertex can't be found in start vertex lav");
+  }
 
-	static MergeBeforeBaseVertex(base, merged) {
-		const size = merged.List.Size;
+  static MergeBeforeBaseVertex(base, merged) {
+    const size = merged.List.Size;
 
-		for (let i = 0; i < size; i++) {
-			const nextMerged = merged.Next;
-			nextMerged.Remove();
+    for (let i = 0; i < size; i++) {
+      const nextMerged = merged.Next;
+      nextMerged.Remove();
 
-			base.AddPrevious(nextMerged);
-		}
-	}
+      base.AddPrevious(nextMerged);
+    }
+  }
 
-	static MoveAllVertexToLavEnd(vertex, newLaw) {
-		const size = vertex.List.Size;
-		for (let i = 0; i < size; i++) {
-			const ver = vertex;
-			vertex = vertex.Next;
-			ver.Remove();
-			newLaw.AddLast(ver);
-		}
-	}
+  static MoveAllVertexToLavEnd(vertex, newLaw) {
+    const size = vertex.List.Size;
+    for (let i = 0; i < size; i++) {
+      const ver = vertex;
+      vertex = vertex.Next;
+      ver.Remove();
+      newLaw.AddLast(ver);
+    }
+  }
 }
