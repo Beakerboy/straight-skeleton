7e573eb14f71ec21aa621d023a2fb3fd0b36e865
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-13 15:43:06 -0400

    Update skeleton.test.js

--- test/skeleton.test.js
+++ test/skeleton.test.js
@@ -4,17 +4,17 @@ import Vector2d from '../src/Primatives/vector2d.js';
 
 test('Circular Add Test', () => {
 
-  const polygon = new List([
-    new Vector2d(50, 50),
-    new Vector2d(100, 50),
-    new Vector2d(100, 100),
-    new Vector2d(50, 100),
-  ]);
+  const polygon = new List();
+  polygon.add(new Vector2d(50, 50));
+  polygon.add(new Vector2d(100, 50));
+  polygon.add(new Vector2d(100, 100));
+  polygon.add(new Vector2d(50, 100));
 
-  var expected = new List([new Vector2d(75.000000, 75.000000)]);
-  expected.AddRange(polygon);
+  const expected = new List();
+  expected.add(new Vector2d(75.000000, 75.000000)]);
+  expected.addRange(polygon);
 
-  var sk = SkeletonBuilder.Build(polygon);
+  const sk = SkeletonBuilder.build(polygon);
 
   SkeletonTestUtil.AssertExpectedPoints(expected, SkeletonTestUtil.GetFacePoints(sk));
 });
