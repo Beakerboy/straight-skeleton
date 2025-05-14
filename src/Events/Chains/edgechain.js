ffe1ccfcbd79d4bc2e7aae2f4ee67d495d0ed196
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-14 12:14:19 -0400

    Update edgechain.js

--- src/Events/Chains/edgechain.js
+++ src/Events/Chains/edgechain.js
@@ -4,32 +4,63 @@ const Edge = require('../../Circular/edge')
 const Vertex = require('../../Circular/vertex')
 const ChainType = require('./chaintype')
 
-class EdgeChain {
+export default class EdgeChain {
+  /**
+   * @type {boolean}
+   */
+  _closed: boolean;
+
+  /**
+   * @type {List}
+   */
+  edgeList
+
+  /**
+   * @param {EdgeList} edgeList List of edges
+   */
   constructor (edgeList) {
     this.edgeList = edgeList
     this._closed = this.previousVertex === this.nextVertex
   }
 
+  /**
+   * @returns {EdgeEvent}
+   */
   get previousEdge () {
     return this.edgeList[0].previousVertex.previousEdge
   }
 
+  /**
+   * @returns {EdgeEvent}
+   */
   get nextEdge () {
     return this.edgeList[this.edgeList.count - 1].nextVertex.nextEdge
   }
 
+  /**
+   * @returns {Vertex}
+   */
   get previousVertex () {
     return this.EdgeList[0].PreviousVertex
   }
 
+  /**
+   * @returns {Vertex}
+   */
   get nextVertex () {
     return this.EdgeList[this.EdgeList.Count - 1].NextVertex
   }
 
+  /**
+   * @returns {Vertex}
+   */
   get currentVertex () {
     return null
   }
 
+  /**
+   * @returns {ChainType}
+   */
   get chainType () {
     return this._closed ? ChainType.CLOSED_EDGE : ChainType.EDGE
   }
