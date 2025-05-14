d95e1fdcebd2e212bd268f76df58a406c5115715
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-14 12:02:58 -0400

    Update circularnode.js

--- src/Circular/circularnode.js
+++ src/Circular/circularnode.js
@@ -1,35 +1,35 @@
 export default class CircularNode {
   /**
-   *
+   * @type {CircularList}
    */
   list
 
   /**
-   *
+   * @type {CircularNode}
    */
   next
 
   /**
-   *
+   * @type {CircularNode}
    */
   previous
 
   /**
-   *
+   * @param {CircularNode} node New Node
    */
   addNext (node) {
-    this.list.addNext(node)
+    this.list.addNext(this, node)
   }
 
   /**
-   *
+   * @param {CircularNode} node New Node
    */
   addPrevious (node) {
-    this.list.addPrevious(node)
+    this.list.addPrevious(this, node)
   }
 
   /**
-   *
+   * Remove a node
    */
   remove () {
     this.list.remove(this)
