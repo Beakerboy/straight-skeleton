6d73db93b89b03431e53de07a4bc4d27172bd126
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-14 09:26:39 -0400

    Update lineparametric2d.js

--- src/Primitives/lineparametric2d.js
+++ src/Primitives/lineparametric2d.js
@@ -2,17 +2,33 @@ import Vector2d from './vector2d';
 import LineLinear2d from './linelinear2d';
 import PrimitiveUtils from './primitiveutils';
 
+/**
+ * Why do we need two forms of a linear model?
+ * Does this define a line segment while LineLinear is a ray?
+ * #JustifyYourExistance
+ */
 export default class LineParametric2d {
   static empty = new LineParametric2d(Vector2d.empty, Vector2d.empty);
 
+  // @type {Vector2d}
   a = null;
+  // @type {Vector2d}
   u = null;
 
+  /**
+   *
+   * @param {Vector2d}
+   * @param {Vector2d}
+   */
   constructor(pA, pU) {
     this.a = pA;
     this.u = pU;
   }
 
+  /**
+   *
+   * @return {LineLinear2d}
+   */
   createLinearForm() {
     const x = this.a.x;
     const y = this.a.y;
@@ -25,6 +41,14 @@ export default class LineParametric2d {
     return new LineLinear2d().setFromCoefficients(a, b, c);
   }
 
+  /**
+   *
+   *
+   * @param {LineParametric2d}
+   * @param {LineLinear2d}
+   * @param {number}
+   * @return {Vector2d}
+   */
   static collide(ray, line, epsilon) {
     const collide = LineLinear2d.collide(ray.createLinearForm(), line);
     if (collide.equals(Vector2d.empty)) {
@@ -35,11 +59,21 @@ export default class LineParametric2d {
     return ray.u.dot(collideVector) < epsilon ? Vector2d.empty : collide;
   }
 
+  /**
+   * @param {Vector2d}
+   * @param {number}
+   * @return {boolean}
+   */
   isOnLeftSite(point, epsilon) {
     const direction = point.sub(this.a);
     return PrimitiveUtils.orthogonalRight(this.u).dot(direction) < epsilon;
   }
 
+  /**
+   * @param {Vector2d}
+   * @param {number}
+   * @return {boolean}
+   */
   isOnRightSite(point, epsilon) {
     const direction = point.sub(this.a);
     return PrimitiveUtils.orthogonalRight(this.u).dot(direction) > -epsilon;
