4ca53c29d6e820942f8b9cc84add9cc688016745
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-15 13:42:10 -0400

    Update skeletonbuilder.js

--- src/skeletonbuilder.js
+++ src/skeletonbuilder.js
@@ -1,5 +1,5 @@
 import { Skeleton } from './skeleton'
-import { HashSet, List, Dictionary} from './utils'
+import { HashSet, List, Dictionary } from './utils'
 import Vector2d from './Primitives/vector2d.js'
 import PriorityQueue from './Primitives/priorityqueue'
 import Edge from './Circular/edge'
