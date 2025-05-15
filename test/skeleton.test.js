5d40dd653531e639a1b16fdaad3ec7b67d3784bc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-15 13:47:14 -0400

    Update skeleton.test.js

--- test/skeleton.test.js
+++ test/skeleton.test.js
@@ -1,4 +1,4 @@
-import { SkeletonBuilder } from '../src/skeletonbuilder.js'
+import SkeletonBuilder from '../src/skeletonbuilder.js'
 import { List } from '../src/utils.js'
 import Vector2d from '../src/Primitives/vector2d.js'
 import SkeletonTestUtil from './skeletontestutil.js'
