b816f9f3a2bb00f48168cb07cacf9644b03738b5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-15 07:30:17 -0400

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 /**
- * insert the specified item at the specified position in 
+ * insert the specified item at the specified position in
  * provided array.
  * @param {Array} array The array
  * @param {number} index Position
- * @param {Any} item The item
+ * @param {any} item The item
  * @returns {Array} The new array
  */
 function insertInArray (array, index, item) {
