d56132018dba74b6c43a298ed90a8c876e3cb899
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-15 17:47:27 -0400

    Update edge.test.js

--- test/edge.test.js
+++ test/edge.test.js
@@ -2,8 +2,8 @@ import Edge from '../src/Circular/edge.js'
 import Vector2d from '../src/Primitives/vector2d.js'
 
 test('Constructor', () => {
-  const v1 = Vector2d(0, 0)
-  const v2 = Vector2d(1, 1)
+  const v1 = new Vector2d(0, 0)
+  const v2 = new Vector2d(1, 1)
   const edge = new Edge(v1, v2)
 
   expect(edge.begin.toString()).toBe('0, 0')
