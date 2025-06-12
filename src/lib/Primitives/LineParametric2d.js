8489a07ee618e50264744e5bab4caa91e457a6a2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-12 10:56:57 -0400

    Update LineParametric2d.js

--- src/lib/Primitives/LineParametric2d.js
+++ src/lib/Primitives/LineParametric2d.js
@@ -3,7 +3,7 @@ import LineLinear2d from "./LineLinear2d";
 import PrimitiveUtils from "./PrimitiveUtils";
 
 export default class LineParametric2d {
-	 static readonly Empty = new LineParametric2d(Vector2d.Empty, Vector2d.Empty);
+	 static Empty = new LineParametric2d(Vector2d.Empty, Vector2d.Empty);
 
 	 A = null;
 	 U = null;
