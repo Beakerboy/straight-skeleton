e72660a15c2a5ba2395f8a3aa8dd53936a48fcb0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-12 10:15:12 -0400

    Update vector2d.test.js

--- test/Primitives/vector2d.test.js
+++ test/Primitives/vector2d.test.js
@@ -2,13 +2,13 @@ import Vector2d from '../../src/lib/Primitives/Vector2d'
 
 test('Constructor', () => {
   const v = new Vector2d(0, 0)
-  expect(v.x).toBe(0)
-  expect(v.y).toBe(0)
+  expect(v.X).toBe(0)
+  expect(v.Y).toBe(0)
 })
 
 test('Negate', () => {
   const v = new Vector2d(1, 1)
-  v.negate()
+  v.Negate()
   expect(v.x).toBe(-1)
   expect(v.y).toBe(-1)
 })
@@ -16,13 +16,13 @@ test('Negate', () => {
 test('Distance To', () => {
   const v1 = new Vector2d(0, 0)
   const v2 = new Vector2d(1, 1)
-  const result = v2.distanceTo(v1)
+  const result = v2.DistanceTo(v1)
   expect(result).toBe(Math.sqrt(2))
 })
 
 test('Normalized', () => {
   const v1 = new Vector2d(5, 0)
-  const result = v1.normalized(v1)
+  const result = v1.Normalized(v1)
   expect(result.x).toBe(1)
   expect(result.y).toBe(0)
 })
@@ -30,57 +30,57 @@ test('Normalized', () => {
 test('Dot', () => {
   const v1 = new Vector2d(1, 2)
   const v2 = new Vector2d(4, 3)
-  const result = v1.dot(v2)
+  const result = v1.Dot(v2)
   expect(result).toBe(10)
 })
 
 test('Distance Squared', () => {
   const v1 = new Vector2d(0, 0)
   const v2 = new Vector2d(1, 1)
-  const result = v2.distanceSquared(v1)
+  const result = v2.DistanceSquared(v1)
   expect(result).toBe(2)
 })
 
 test('Add', () => {
   const v1 = new Vector2d(1, 2)
   const v2 = new Vector2d(4, 3)
-  const result = v1.add(v2)
-  expect(result.x).toBe(5)
-  expect(result.y).toBe(5)
+  const result = v1.Add(v2)
+  expect(result.X).toBe(5)
+  expect(result.Y).toBe(5)
 })
 
 test('Sub', () => {
   const v1 = new Vector2d(1, 2)
   const v2 = new Vector2d(4, 3)
-  const result = v2.sub(v1)
-  expect(result.x).toBe(3)
-  expect(result.y).toBe(1)
+  const result = v2.Sub(v1)
+  expect(result.X).toBe(3)
+  expect(result.Y).toBe(1)
 })
 
 test('Add', () => {
   const v1 = new Vector2d(1, 2)
-  const result = v1.multiplyScalar(3)
-  expect(result.x).toBe(3)
-  expect(result.y).toBe(6)
+  const result = v1.MultiplyScalar(3)
+  expect(result.X).toBe(3)
+  expect(result.Y).toBe(6)
 })
 
 test('Equals', () => {
   const v1 = new Vector2d(1, 2)
-  const result = v1.equals(v1)
+  const result = v1.Equals(v1)
   expect(result).toBe(true)
 })
 
 test('Not Equals', () => {
   const v1 = new Vector2d(1, 2)
   const v2 = new Vector2d(2, 1)
-  let result = v1.notEquals(v2)
+  let result = v1.NotEquals(v2)
   expect(result).toBe(true)
-  result = v1.notEquals(v1)
+  result = v1.NotEquals(v1)
   expect(result).toBe(false)
 })
 
 test('To String', () => {
   const v1 = new Vector2d(1, 2)
-  const result = v1.toString()
+  const result = v1.ToString()
   expect(result).toBe('1, 2')
 })
