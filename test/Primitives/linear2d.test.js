182068e3c45464f54f5798a8d0daf1f943ba5d69
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-12 10:47:40 -0400

    Create linear2d.test.js

---
+++ test/Primitives/linear2d.test.js
@@ -0,0 +1,23 @@
+import LineLinear2d from '../../src/lib/Primitives/LineLinear2d'
+import Vector2d from '../../src/lib/Primitives/Vector2d'
+
+test('Comstructor', () => {
+  const v1 = new Vector2d(0, 0)
+  const v2 = new Vector2d(1, 1)
+  const lin = new LineLinear2d(v1, v2)
+  expect(lin.A).toBe(-1)
+  expect(lin.B).toBe(1)
+  expect(lin.C).toBe(0)
+})
+
+test('Factory', () => {
+
+})
+
+test('Collide', () => {
+
+})
+
+test('Contains', () => {
+
+})
