c47ae442dd41bb9f55217968920b9d0a706d9ad6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-12 09:58:42 -0400

    Create edge.test.js

---
+++ test/Circular/edge.test.js
@@ -0,0 +1,24 @@
+import Edge from '../../src/Circular/Edge.js'
+import Vector2d from '../../src/Primitives/Vector2d.js'
+
+const v1 = new Vector2d(0, 0)
+const v2 = new Vector2d(1, 1)
+const edge = new Edge(v1, v2)
+
+test('Constructor', () => {
+  expect(edge.Begin.toString()).toBe('0, 0')
+  expect(edge.End.toString()).toBe('1, 1')
+})
+
+test('Norm', () => {
+
+})
+
+test('Line', () => {
+
+})
+
+test('To String', () => {
+  const expected = 'Edge [p1=0, 0, p2=1, 1]'
+  expect(edge.ToString()).toBe(expected)
+})
