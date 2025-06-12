aad6573ab0b900547466e68d722148a2fddf81a8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-12 12:41:51 -0400

    Update linear2d.test.js

--- test/Primitives/linear2d.test.js
+++ test/Primitives/linear2d.test.js
@@ -23,6 +23,13 @@ test('Collide', () => {
   expect(vec.Y).toBe(-4);
 })
 
+test('Collide parallel', () => {
+  const lin1 = new LineLinear2d().SetFromCoefficients(1, 0, 3);
+  const lin2 = new LineLinear2d().SetFromCoefficients(1, 0, 4);
+  const vec = lin1.Collide(lin2);
+  expect(vec.Equals(Vector2d.Empty)).toBe(true);
+})
+
 test('Contains', () => {
 
 })
