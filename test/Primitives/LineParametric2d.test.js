b533ac1c8c60e666d51b3300dc1b54438b38cea5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-12 20:44:52 -0400

    Update LineParametric2d.test.js

--- test/Primitives/LineParametric2d.test.js
+++ test/Primitives/LineParametric2d.test.js
@@ -24,7 +24,7 @@ test('Collide', () => {
   const r1 = new LineParametric2d(v1, v2);
   const l1 = new LineLinear2d(v2, v3);
   const epsilon = .0001;
-  const result = LineParametric2d.Collide(ray, line, epsilon);
+  const result = LineParametric2d.Collide(r1, l1, epsilon);
   expect(result.X).toBe(1);
 })
 
