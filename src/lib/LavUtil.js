3c89bfd188bb32dc37702aab5132357893c0cd28
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-13 08:05:43 -0400

    Update and rename LavUtil.ts to LavUtil.js

--- src/lib/LavUtil.js
+++ src/lib/LavUtil.js
@@ -3,19 +3,19 @@ import {List} from "./Utils";
 import CircularList from "./Circular/CircularList";
 
 export default class LavUtil {
-	public static IsSameLav(v1: Vertex, v2: Vertex): boolean {
+	IsSameLav(v1, v2) {
 		if (v1.List === null || v2.List === null)
 			return false;
 		return v1.List === v2.List;
 	}
 
-	public static RemoveFromLav(vertex: Vertex) {
+	RemoveFromLav(vertex) {
 		if (vertex === null || vertex.List === null)
 			return;
 		vertex.Remove();
 	}
 
-	public static CutLavPart(startVertex: Vertex, endVertex: Vertex): List<Vertex> {
+	CutLavPart(startVertex, endVertex) {
 		const ret = new List<Vertex>();
 		const size = startVertex.List.Size;
 		let next = startVertex;
@@ -33,7 +33,7 @@ export default class LavUtil {
 		throw new Error("End vertex can't be found in start vertex lav");
 	}
 
-	public static MergeBeforeBaseVertex(base: Vertex, merged: Vertex) {
+	MergeBeforeBaseVertex(base, merged) {
 		const size = merged.List.Size;
 
 		for (let i = 0; i < size; i++) {
@@ -44,7 +44,7 @@ export default class LavUtil {
 		}
 	}
 
-	public static MoveAllVertexToLavEnd(vertex: Vertex, newLaw: CircularList<Vertex>) {
+	MoveAllVertexToLavEnd(vertex, newLaw) {
 		const size = vertex.List.Size;
 		for (let i = 0; i < size; i++) {
 			const ver = vertex;
