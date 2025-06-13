06802af35284c5ca445a80a03904560cf6f415cd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-13 09:39:17 -0400

    Update MultiSplitEvent.js

--- src/lib/Events/MultiSplitEvent.js
+++ src/lib/Events/MultiSplitEvent.js
@@ -1,6 +1,5 @@
 import SkeletonEvent from "./SkeletonEvent";
 import {List} from "../Utils";
-import IChain from "./Chains/IChain";
 import Vector2d from "../Primitives/Vector2d";
 
 export default class MultiSplitEvent extends SkeletonEvent {
