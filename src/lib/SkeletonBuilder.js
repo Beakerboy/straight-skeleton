5ec68f19be53f7005ab0ced78397744fbbf1c93a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-14 10:16:58 -0400

    Update SkeletonBuilder.js

--- src/lib/SkeletonBuilder.js
+++ src/lib/SkeletonBuilder.js
@@ -578,7 +578,7 @@ export default class SkeletonBuilder {
 		if (holes === null)
 			return null;
 
-		const ret = new List(holes.Count);
+		const ret = new List(holes.length);
 		for (const hole of holes) {
 			if (PrimitiveUtils.IsClockwisePolygon(hole))
 				ret.push(hole);
