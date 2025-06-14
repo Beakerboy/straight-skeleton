798bf373af9b775a5b0a5c4ec3147c054a48bf81
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-14 09:44:28 -0400

    Update PriorityQueue.js

--- src/lib/Primitives/PriorityQueue.js
+++ src/lib/Primitives/PriorityQueue.js
@@ -27,11 +27,11 @@ export default class PriorityQueue {
 	}
 
 	get Count() {
-		return this._heap.Count;
+		return this._heap.length;
 	}
 
 	get Empty() {
-		return this._heap.Count === 0;
+		return this._heap.length === 0;
 	}
 
 	Peek() {
@@ -40,7 +40,7 @@ export default class PriorityQueue {
 
 	Next() {
 		const val = this._heap[0];
-		const nMax = this._heap.Count - 1;
+		const nMax = this._heap.length - 1;
 		this._heap[0] = this._heap[nMax];
 		this._heap.RemoveAt(nMax);
 
