34b2834899f9d3c1ceefcf3d4a75d8d476d07300
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-14 11:23:57 -0400

    update

--- src/lib/SkeletonBuilder.js
+++ src/lib/SkeletonBuilder.js
@@ -398,7 +398,7 @@ export default class SkeletonBuilder {
 		splitEventLoop:
 			while (splitCluster.Any()) {
 				const split = splitCluster[0];
-				splitCluster.RemoveAt(0);
+				splitCluster.splice(0, 1);
 
 				for (const chain of edgeChains) {
 					if (this.IsInEdgeChain(split, chain))
