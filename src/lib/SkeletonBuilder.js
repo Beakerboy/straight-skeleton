c98a75007fdf0f2c03505be7bd237e4d66d1fbd2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-14 09:50:05 -0400

    Update SkeletonBuilder.js

--- src/lib/SkeletonBuilder.js
+++ src/lib/SkeletonBuilder.js
@@ -426,7 +426,7 @@ export default class SkeletonBuilder {
 		loop:
 			for (; ;) {
 				const beginVertex = edgeList[0].PreviousVertex;
-				const endVertex = edgeList[edgeList.Count - 1].NextVertex;
+				const endVertex = edgeList[edgeList.length - 1].NextVertex;
 
 				for (let i = 0; i < edgeCluster.Count; i++) {
 					const edge = edgeCluster[i];
