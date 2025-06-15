fd4eb1b305625ad93d3855de4baa76d1c415fddf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-14 20:40:41 -0400

    Update Utils.js

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -1,12 +1,2 @@
-export class List extends Array {
-
-	Remove(itemToRemove) {
-		const newArr = this.filter(item => item !== itemToRemove);
-
-		this.length = newArr.length;
-
-		for(let i = 0; i < newArr.length; i++) {
-			this[i] = newArr[i];
-		}
-	}
+export class List extends Array {	
 }
