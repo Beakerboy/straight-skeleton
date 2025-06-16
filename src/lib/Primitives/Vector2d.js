36c955c9d41ddedaa2a2802a91db01d25289839d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-15 20:20:27 -0400

    Update Vector2d.js

--- src/lib/Primitives/Vector2d.js
+++ src/lib/Primitives/Vector2d.js
@@ -19,10 +19,6 @@ export default class Vector2d extends Vector2 {
 		this.y = value;
 	}
 
-	 Sub(v) {
-		return this.clone().sub(v)
-	}
-
 	 MultiplyScalar(scale) {
 		return this.clone().multiplyScalar(scale)
 	}
