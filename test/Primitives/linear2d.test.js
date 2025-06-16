b36aff164184d33740d655adaa2c42ee27f489c4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-16 08:10:35 -0400

    Update linear2d.test.js

--- test/Primitives/linear2d.test.js
+++ test/Primitives/linear2d.test.js
@@ -1,9 +1,9 @@
 import LineLinear2d from '../../src/lib/Primitives/LineLinear2d'
-import Vector2d from '../../src/lib/Primitives/Vector2d'
+import {Vector2} from 'three'
 
 test('Comstructor', () => {
-  const v1 = new Vector2d(0, 0)
-  const v2 = new Vector2d(1, 1)
+  const v1 = new Vector2(0, 0)
+  const v2 = new Vector2(1, 1)
   const lin = new LineLinear2d(v1, v2)
   expect(lin.A).toBe(-1)
   expect(lin.B).toBe(1)
@@ -19,20 +19,20 @@ test('Collide', () => {
   const lin1 = new LineLinear2d().SetFromCoefficients(1, 0, 3);
   const lin2 = new LineLinear2d().SetFromCoefficients(0, 1, 4);
   const vec = lin1.Collide(lin2);
-  expect(vec.X).toBe(-3);
-  expect(vec.Y).toBe(-4);
+  expect(vec.x).toBe(-3);
+  expect(vec.y).toBe(-4);
 })
 
 test('Collide parallel', () => {
   const lin1 = new LineLinear2d().SetFromCoefficients(1, 0, 3);
   const lin2 = new LineLinear2d().SetFromCoefficients(1, 0, 4);
   const vec = lin1.Collide(lin2);
-  expect(vec.equals(Vector2d.Empty)).toBe(true);
+  expect(vec.equals(new Vector2(null, null))).toBe(true);
 })
 
 /** @todo test Collide overlapping */
 
 test('Contains', () => {
   const lin1 = new LineLinear2d().SetFromCoefficients(1, 0, 3);
-  expect(lin1.Contains(new Vector2d(-3, 10))).toBe(true)
+  expect(lin1.Contains(new Vector2(-3, 10))).toBe(true)
 })
