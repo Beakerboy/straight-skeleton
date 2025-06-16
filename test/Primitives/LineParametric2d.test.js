7511b488d718f85d86ce534819ed595e51a9617e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-16 08:12:22 -0400

    Update LineParametric2d.test.js

--- test/Primitives/LineParametric2d.test.js
+++ test/Primitives/LineParametric2d.test.js
@@ -1,31 +1,31 @@
 import LineLinear2d from '../../src/lib/Primitives/LineLinear2d'
 import LineParametric2d from '../../src/lib/Primitives/LineParametric2d'
-import Vector2d from '../../src/lib/Primitives/Vector2d'
+import {Vector2} from 'three'
 
 test('Constructor', () => {
-  const v1 = new Vector2d(0, 0)
-  const v2 = new Vector2d(1, 1)
+  const v1 = new Vector2(0, 0)
+  const v2 = new Vector2(1, 1)
   const r1 = new LineParametric2d(v1, v2)
-  expect(r1.A.X).toBe(0)
+  expect(r1.A.x).toBe(0)
 })
 
 test('Create Linear Form', () => {
-  const v1 = new Vector2d(0, 0)
-  const v2 = new Vector2d(1, 1)
+  const v1 = new Vector2(0, 0)
+  const v2 = new Vector2(1, 1)
   const l1 = new LineParametric2d(v1, v2).CreateLinearForm();
   expect(l1.constructor.name).toBe("LineLinear2d");
   expect(l1.Contains(v1)).toBe(true);
 })
 
 test('Collide', () => {
-  const v1 = new Vector2d(0, 0);
-  const v2 = new Vector2d(1, 1);
-  const v3 = new Vector2d(1, 2);
+  const v1 = new Vector2(0, 0);
+  const v2 = new Vector2(1, 1);
+  const v3 = new Vector2(1, 2);
   const r1 = new LineParametric2d(v1, v2);
-  const l1 = new LineLinear2d(v2, v3);
+  const l1 = new LineLinear2(v2, v3);
   const epsilon = .0001;
   const result = LineParametric2d.Collide(r1, l1, epsilon);
-  expect(result.X).toBe(1);
+  expect(result.x).toBe(1);
 })
 
 /** @todo test fail to collide */
@@ -33,18 +33,18 @@ test('Collide', () => {
 /** @todo test colinear lines */
 
 test('Left Side', () => {
-  const v1 = new Vector2d(0, 0)
-  const v2 = new Vector2d(1, 0)
-  const v3 = new Vector2d(1, 1)
+  const v1 = new Vector2(0, 0)
+  const v2 = new Vector2(1, 0)
+  const v3 = new Vector2(1, 1)
   const r1 = new LineParametric2d(v1, v2)
   const result = r1.IsOnLeftSite(v3, .0001)
   expect(result).toBe(true);
 })
 
 test('Right Side', () => {
-  const v1 = new Vector2d(0, 0)
-  const v2 = new Vector2d(1, 0)
-  const v3 = new Vector2d(1, -1)
+  const v1 = new Vector2(0, 0)
+  const v2 = new Vector2(1, 0)
+  const v3 = new Vector2(1, -1)
   const r1 = new LineParametric2d(v1, v2)
   const result = r1.IsOnRightSite(v3, .0001)
   expect(result).toBe(true);
