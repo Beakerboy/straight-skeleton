86fc8eee54aecacb4083d9384d319d29787b8cbf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-19 09:21:59 -0400

    Update LineParametric2d.test.js

--- test/Primitives/LineParametric2d.test.js
+++ test/Primitives/LineParametric2d.test.js
@@ -1,18 +1,18 @@
 import LineLinear2d from '../../src/lib/Primitives/LineLinear2d'
-import LineParametric2d from '../../src/lib/Primitives/LineParametric2d'
+import Ray2 from '../../src/lib/Primitives/Ray2'
 import {Vector2} from 'three'
 
 test('Constructor', () => {
   const v1 = new Vector2(0, 0)
   const v2 = new Vector2(1, 1)
-  const r1 = new LineParametric2d(v1, v2)
+  const r1 = new Ray2(v1, v2)
   expect(r1.origin.x).toBe(0)
 })
 
 test('Create Linear Form', () => {
   const v1 = new Vector2(0, 0)
   const v2 = new Vector2(1, 1)
-  const l1 = new LineParametric2d(v1, v2).CreateLinearForm();
+  const l1 = new Ray2(v1, v2).CreateLinearForm();
   expect(l1.constructor.name).toBe("LineLinear2d");
   expect(l1.Contains(v1)).toBe(true);
 })
@@ -21,10 +21,10 @@ test('Collide', () => {
   const v1 = new Vector2(0, 0);
   const v2 = new Vector2(1, 1);
   const v3 = new Vector2(1, 2);
-  const r1 = new LineParametric2d(v1, v2);
+  const r1 = new Ray2(v1, v2);
   const l1 = new LineLinear2d(v2, v3);
   const epsilon = .0001;
-  const result = LineParametric2d.Collide(r1, l1, epsilon);
+  const result = Ray2.Collide(r1, l1, epsilon);
   expect(result.x).toBe(1);
 })
 
@@ -36,7 +36,7 @@ test('Left Side', () => {
   const v1 = new Vector2(0, 0)
   const v2 = new Vector2(1, 0)
   const v3 = new Vector2(1, 1)
-  const r1 = new LineParametric2d(v1, v2)
+  const r1 = new Ray2(v1, v2)
   const result = r1.IsOnLeftSite(v3, .0001)
   expect(result).toBe(true);
 })
@@ -45,7 +45,7 @@ test('Right Side', () => {
   const v1 = new Vector2(0, 0)
   const v2 = new Vector2(1, 0)
   const v3 = new Vector2(1, -1)
-  const r1 = new LineParametric2d(v1, v2)
+  const r1 = new Ray2(v1, v2)
   const result = r1.IsOnRightSite(v3, .0001)
   expect(result).toBe(true);
 })
