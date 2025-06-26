3336ed3bfac74467acd8af32a00415129663a5c6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-26 12:51:34 -0400

    Update FaceQueue.js

--- src/lib/Path/FaceQueue.js
+++ src/lib/Path/FaceQueue.js
@@ -13,7 +13,7 @@ export default class FaceQueue extends PathQueue {
 
 	AddPush(node, newNode) {
 		if (this.Closed)
-			throw new Error("Can't add node to closed FaceQueue");
+			throw new Error("Can't add node to closed FaceQueue Node: " + node.ToString() + " NewNode: " + newNode.ToString());
 
 		super.AddPush(node, newNode);
 	}
