bb5270d5ba738c98f1bd63127caabe9243529cdd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-27 07:36:38 -0400

    Update edge.test.js

--- test/Circular/edge.test.js
+++ test/Circular/edge.test.js
@@ -6,8 +6,8 @@ const v2 = new Vector2d(1, 1)
 const edge = new Edge(v1, v2)
 
 test('Constructor', () => {
-  expect(edge.Begin.ToString()).toBe('0, 0')
-  expect(edge.End.ToString()).toBe('1, 1')
+  expect(edge.Begin.toString()).toBe('0, 0')
+  expect(edge.End.toString()).toBe('1, 1')
 })
 
 test('Norm', () => {
@@ -20,5 +20,5 @@ test('Line', () => {
 
 test('To String', () => {
   const expected = 'Edge [p1=0, 0, p2=1, 1]'
-  expect(edge.ToString()).toBe(expected)
+  expect(edge.toString()).toBe(expected)
 })
